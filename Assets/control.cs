288ae46ee8a1bd37429069cb788749d74afadb01
Haman0w0 <haman0w0@outlook.com>
2025-09-17 22:47:54 +0800

    first update
    第一次完整上傳

---
+++ Assets/control.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            player.transform.Translate(0, 0, 0.5f);
+        }
+        if(Input.GetKey("s"))
+        {
+            player.transform.Translate(0, 0, -0.5f);
+        }
+        if(Input.GetKey("a"))
+        {
+            player.transform.Translate(-0.5f, 0, 0);
+        }
+        if(Input.GetKey("d"))
+        {
+            player.transform.Translate(0.5f, 0, 0);
+        }
+    }
+}
